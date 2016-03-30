777107fd55fc886864c59deb1d97322810cc8773
Brant Fitzsimmons <brant.fitzsimmons@gmail.com>
2016-03-30 12:39:39 -0400

    Make the linter happy.

--- util/util.go
+++ util/util.go
@@ -15,15 +15,14 @@ type Logger struct {
 
 var LOGGER = &Logger{log.New(os.Stderr, "", log.LstdFlags)}
 
-// fatal is like panic except it displays only the current goroutine's stack.
+// Fatal is like panic except it displays only the current goroutine's stack.
 func Fatal(format string, v ...interface{}) {
 	// https://github.com/ActiveState/log/blob/master/log.go#L45
 	LOGGER.Output(2, fmt.Sprintf("FATAL -- "+format, v...)+"\n"+string(debug.Stack()))
 	os.Exit(1)
 }
 
-// partitionString partitions the string into chunks of given size,
-// with the last chunk of variable size.
+// PartitionString partitions the string into chunks of a given size, with the last chunk of variable size.
 func PartitionString(s string, chunkSize int) []string {
 	if chunkSize <= 0 {
 		panic("invalid chunkSize")
